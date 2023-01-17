a32a2e4db0c7c59fbac0f81f6784cee894dbf32b
0xhrsh <github@0xhrsh.com>
2023-01-18 00:56:33 +0530

    increases blur

--- src/const.js
+++ src/const.js
@@ -5,4 +5,4 @@ export const TARGET_FPS = 10;
 export const BACKEND = 'mediapipe-gpu';
 export const MODEL = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
 export const VIDEO_SIZE = '640 X 480';
-export const BLUR = 0.5;
+export const BLUR = 1;
