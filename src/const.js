5edccdab9b213824e7e6c8845a4b49af594e65b9
0xhrsh <github@0xhrsh.com>
2023-01-18 20:19:00 +0530

    minor changes

--- src/const.js
+++ src/const.js
@@ -7,11 +7,13 @@ export const MODEL = faceLandmarksDetection.SupportedModels.MediaPipeFaceMesh;
 
 export const VIDEO_SIZE = { width: 1920, height: 1080 };
 export const MOBILE_VIDEO_SIZE = { width: 360, height: 270 };
-export const TARGET_FPS = 20;
+// export const MOBILE_VIDEO_SIZE = { width: 1920, height: 1080 };
+export const TARGET_FPS = 15;
 
-export const MAX_ALLOWED_OFFSET = 1.7;
-export const BLUR = 1;
+export const MAX_ALLOWED_OFFSET = 2.5;
+export const BLUR = 1.25;
 
+export const LIPSTICK_TRANSPARENCY = 0.25;
 export const LIPSTICK_COLORS = {
     'Sinful Cherry': [170, 0, 52],
     'Delicious Plum': [127, 2, 75]
