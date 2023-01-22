5069f6f31616bfa6c7885558bb933761e3f7e56f
0xhrsh <github@0xhrsh.com>
2023-01-22 11:35:15 +0530

    reduces broadness

--- src/const.js
+++ src/const.js
@@ -17,4 +17,4 @@ export const LIPSTICK_COLORS = {
     'Sinful Cherry': [170, 0, 52],
     'Delicious Plum': [127, 2, 75]
 }
-export const EXTENSION_DELTA = 0.25;
+export const EXTENSION_DELTA = 0.17;
